6872ba35683c14ecfc01fc20c699cab7628a1a87
wlozan00 <william.lozano20004@gmail.com>
2022-03-15 19:27:14 +0100

    prvi commit

---
+++ Vjezba1_wlozan00/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Vjezba1_wlozan00
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

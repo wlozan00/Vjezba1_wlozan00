d0a258154aba29c9228b6331dc556974fe9c26c1
wlozan00 <william.lozano20004@gmail.com>
2022-03-15 19:38:10 +0100

    drugi commit

--- Vjezba1_wlozan00/Program.cs
+++ Vjezba1_wlozan00/Program.cs
@@ -7,6 +7,7 @@ namespace Vjezba1_wlozan00
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Novo");
         }
     }
 }
